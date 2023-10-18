4e6355e527e80765174b3ba2e58a9e7bf829a107
eduarda <eduardacarvalho9833@gmail.com>
2023-10-17 23:07:35 -0300

    feat: challenge sorting people

---
+++ src/main/java/org/miawe/Sorting/People.java
@@ -0,0 +1,48 @@
+package main.java.org.miawe.Sorting;
+
+import java.util.Comparator;
+
+public class People implements Comparable<People>{
+    private String name;
+    private int age;
+    private double height;
+
+    public People(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    @Override
+    public int compareTo(People p) {
+        return Integer.compare(age, p.getAge());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+}
+
+class ComparatorByHeight implements Comparator<People>{
+    @Override
+    public int compare(People p1, People p2) {
+        return Double.compare(p1.getHeight(), p2.getHeight());
+    }
+}
---
+++ src/main/java/org/miawe/Sorting/SortingPeople.java
@@ -0,0 +1,30 @@
+package main.java.org.miawe.Sorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingPeople {
+
+    private List<People> peopleList;
+
+    public SortingPeople() {
+        this.peopleList = new ArrayList<>();
+    }
+
+    public void addPeople(String name, int age, double height){
+        peopleList.add(new People(name, age, height));
+    }
+
+    public List<People> sortingByAge(){
+        List<People> peopleByAge = new ArrayList<>(peopleList);
+        Collections.sort(peopleByAge);
+        return peopleByAge;
+    }
+
+    public List<People> sortingByHeight(){
+        List<People> peopleByHeight = new ArrayList<>(peopleList);
+        peopleByHeight.sort(new ComparatorByHeight());
+        return peopleByHeight;
+    }
+}
